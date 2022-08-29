2f29156f6b5747054a3d110dffcf17596f149900
mateosuster <mateosuster@gmail.com>
2022-08-29 11:40:13 -0300

    var selection bib v1

--- code/01.getData.R
+++ code/01.getData.R
@@ -1,20 +1,19 @@
 library(bib2df)
 library(tidyverse)
 
-path <- system.file("extdata", "data/anthology+abstracts.bib", package = "bib2df")
 
 df <- bib2df("data/anthology+abstracts.bib")
-df <- bib2df(path)
 
 # explore vars
 glimpse(df)
 
 # filter
 dfSel <- df %>% 
-  select(YEAR, TITLE, ABSTRACT) %>% 
+  select(YEAR, TITLE, ABSTRACT,  PUBLISHER, BOOKTITLE, CATEGORY) %>% 
   na.omit()
 
 # EDA
+glimpse(dfSel)
 summary(dfSel)
      
 ## missings   
